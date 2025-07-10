5b01167e89a7216588cc56101cfaa8def90d2b65
Jedidiah Chibinga <jedidiah.gna@gmail.com>
2025-07-10 17:41:02 +0200

    added initial dropdowns

--- apps/home.py
+++ apps/home.py
@@ -1,16 +1,55 @@
 import streamlit as st
 import leafmap.foliumap as leafmap
+import geopandas as gpd
 
+# import farms vector file as a gdf
+farms_df = gpd.read_file(r"data\vector\ce_farms.gpkg")
+farms = farms_df[['farmer', 'crop', 'district', 'province', 'area_ha', 'geometry']]
+
+def add_selectors(farms):
+    # create list of farms
+    farm_names = list(set(farms["farmer"].to_list()))
+
+    # normalised difference indices
+    indices_list = ['Crop Health', 'Soil Moisture', ]
+
+    # specify columns and their widths
+    farm_names_col, indices_list_col, max_cloud_cover_col = st.columns([3, 3, 3])
+
+    with farm_names_col:
+        selected_name = st.selectbox(
+            "Select the farm to monitor",
+            farm_names,
+            index=farm_names.index("ADM Farm"),
+        )
+
+    with indices_list_col:
+        selected_index = st.selectbox(
+            "Select the metric to monitor",
+            indices_list,
+            index=indices_list.index("Crop Health"),
+        )
+
+    with max_cloud_cover_col:
+        # max_cloud_cover = st.text_input('Select maximum cloud cover', 
+                                    # 0, 100, 20, step=5)
+        max_cloud_cover = st.slider('Select maximum cloud cover', 
+                                    0, 100, 20, step=5)
+        
+        return farm_names_col, indices_list_col, max_cloud_cover_col
 
 def app():
-    st.title("Home")
+    st.title("Vegetation Health Monitor")
 
     st.markdown(
         """
     A [streamlit](https://streamlit.io) app for geospatial monitoring of crop health using Sentinel 2 imagery.
     """
     )
+    
+    add_selectors(farms)
 
-    m = leafmap.Map(center= [-13, 28], zoom= 6)
+    m = leafmap.Map(center= [-13.2, 28], zoom= 7)
     m.add_basemap("ROADMAP")
+    m.add_gdf(farms, zoom_to_layer=False, layer_name="Farms")
     m.to_streamlit(height=700)
