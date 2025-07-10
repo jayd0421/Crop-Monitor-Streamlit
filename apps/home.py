37be704630706d3a4c16c7c844623b801224ebb9
Jedidiah Chibinga <jedidiah.gna@gmail.com>
2025-07-10 19:34:07 +0200

    added functionality to zoom to selected farm

--- apps/home.py
+++ apps/home.py
@@ -14,29 +14,33 @@ def add_selectors(farms):
     indices_list = ['Crop Health', 'Soil Moisture', ]
 
     # specify columns and their widths
-    farm_names_col, indices_list_col, max_cloud_cover_col = st.columns([3, 3, 3])
+    farm_names_col, indices_list_col, max_cloud_cover_col, submit_button_col = st.columns([3, 3, 3, 1.5])
 
     with farm_names_col:
-        selected_name = st.selectbox(
+        farm_name = st.selectbox(
             "Select the farm to monitor",
             farm_names,
-            index=farm_names.index("ADM Farm"),
+            index=None,
+            placeholder="Select farm..."
         )
 
     with indices_list_col:
         selected_index = st.selectbox(
             "Select the metric to monitor",
             indices_list,
-            index=indices_list.index("Crop Health"),
+            index=None,
+            placeholder="Select metric..."
         )
 
     with max_cloud_cover_col:
-        # max_cloud_cover = st.text_input('Select maximum cloud cover', 
+        # max_cloud_cover = st.text_input('Select maximum cloud cover',
                                     # 0, 100, 20, step=5)
-        max_cloud_cover = st.slider('Select maximum cloud cover', 
+        max_cloud_cover = st.slider('Select maximum cloud cover',
                                     0, 100, 20, step=5)
-        
-        return farm_names_col, indices_list_col, max_cloud_cover_col
+
+    # with submit_button_col:
+    #     submit_button = st.button("Submit")
+    return farm_name, selected_index, max_cloud_cover#, submit_button_col
 
 def app():
     st.title("Vegetation Health Monitor")
@@ -46,10 +50,18 @@ def app():
     A [streamlit](https://streamlit.io) app for geospatial monitoring of crop health using Sentinel 2 imagery.
     """
     )
-    
-    add_selectors(farms)
 
-    m = leafmap.Map(center= [-13.2, 28], zoom= 7)
-    m.add_basemap("ROADMAP")
-    m.add_gdf(farms, zoom_to_layer=False, layer_name="Farms")
-    m.to_streamlit(height=700)
+    farm_name, selected_index, max_cloud_cover = add_selectors(farms)
+    selected_farm = farms[farms["farmer"] == farm_name]
+
+    if farm_name == None:
+        m = leafmap.Map(center= [-13.2, 28], zoom= 7)
+        m.add_basemap("ROADMAP")
+        m.add_gdf(farms, zoom_to_layer=False, layer_name="Farms")
+        m.to_streamlit(height=700)
+
+    else:
+        m = leafmap.Map()
+        m.add_basemap("ROADMAP")
+        m.add_gdf(selected_farm, zoom_to_layer=True, layer_name="Farms")
+        m.to_streamlit(height=700)
