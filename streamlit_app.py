c77f430fdbd9920fc0666370714a1cd4833a03c1
Jedidiah Chibinga <jedidiah.gna@gmail.com>
2025-07-10 17:10:27 +0200

    commented out code with bug

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,31 +17,26 @@ titles = [app["title"] for app in apps]
 titles_lower = [title.lower() for title in titles]
 icons = [app["icon"] for app in apps]
 
-params = st.experimental_get_query_params()
+# params = st.experimental_get_query_params()
 
-if "page" in params:
-    default_index = int(titles_lower.index(params["page"][0].lower()))
-else:
-    default_index = 0
+# if "page" in params:
+#     default_index = int(titles_lower.index(params["page"][0].lower()))
+# else:
+#     default_index = 0
 
 with st.sidebar:
     selected = option_menu(
-        "Main Menu",
+        "Menu",
         options=titles,
         icons=icons,
         menu_icon="cast",
-        default_index=default_index,
+        # default_index=default_index,
     )
 
     st.sidebar.title("About")
     st.sidebar.info(
         """
-        This web [app](https://share.streamlit.io/giswqs/streamlit-template) is maintained by [Qiusheng Wu](https://wetlands.io). You can follow me on social media:
-            [GitHub](https://github.com/giswqs) | [Twitter](https://twitter.com/giswqs) | [YouTube](https://www.youtube.com/c/QiushengWu) | [LinkedIn](https://www.linkedin.com/in/qiushengwu).
-        
-        Source code: <https://github.com/giswqs/streamlit-template>
-
-        More menu icons: <https://icons.getbootstrap.com>
+        This web [app](https://crop-monitor-app-yyhg7vyflyu7dwisr8hihf.streamlit.app/) is maintained by Jedidiah Chibinga.
     """
     )
 
