5896f87d3101c4e33db27f881a6d1671cc75719d
Jedidiah Chibinga <jedidiah.gna@gmail.com>
2025-07-11 15:46:50 +0200

    updated ee to st function

--- apps/access.py
+++ apps/access.py
@@ -7,7 +7,13 @@ def ee_to_st():
     try:
         ee.Initialize()
     except Exception:
-        service_account_info = st.secrets["earthengine"]
+        # Convert TOML object to a plain Python dict
+        service_account_info = dict(st.secrets["earthengine"])
+        
+        # Convert dict to JSON string
         json_creds = json.dumps(service_account_info)
+        
+        # Authenticate with service account
         credentials = ee.ServiceAccountCredentials(service_account_info["client_email"], key_data=json_creds)
         ee.Initialize(credentials)
+        st.success("✅ Earth Engine initialized using service account.")
